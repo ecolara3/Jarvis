7b18b871d1a2808c58136ccad478d10d8bc66a42
Robert Lara III <robertlaraiii@gmail.com>
2017-12-30 00:51:55 -0500

    Initial Commit

---
+++ rlara.prototypes.jarvis/rlara.prototypes.jarvis/Helpers/Wemo.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rlara.prototypes.jarvis.Helpers
+{
+    public static class Wemo
+    {
+
+        public static async void Toggle(int port)
+        {
+            string uri = $"http://192.168.1.{port}:49153/upnp/control/basicevent1";
+
+            string soapAction = "\"urn:Belkin:service:basicevent:1#GetBinaryState\"";
+
+            string requestBody =
+                "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">\n <s:Body>\n  <u:GetBinaryState xmlns:u=\"urn:Belkin:service:basicevent:1\">\n   \n  </u:GetBinaryState>\n </s:Body>\n</s:Envelope>";
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("SOAPAction", soapAction);
+                var content = new StringContent(requestBody, Encoding.UTF8, "text/xml");
+                using (var response = await client.PostAsync(uri, content))
+                {
+                    Stream receiveStream = await response.Content.ReadAsStreamAsync();
+                    StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                    var text = readStream.ReadToEnd();
+
+                    if (text.Contains("<BinaryState>1</BinaryState>"))
+                    {
+                        Off(port);
+                    }
+                    else
+                    {
+                        On(port);
+                    }
+
+                }
+            }
+
+        }
+
+        public static async void Off(int port)
+        {
+            string uri = $"http://192.168.1.{port}:49153/upnp/control/basicevent1";
+
+            string soapAction = "\"urn:Belkin:service:basicevent:1#SetBinaryState\"";
+
+            string requestBody = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">\n <s:Body>\n  <u:SetBinaryState xmlns:u=\"urn:Belkin:service:basicevent:1\">\n   <BinaryState>0</BinaryState>\n  </u:SetBinaryState>\n </s:Body>\n</s:Envelope>";
+
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("SOAPAction", soapAction);
+                var content = new StringContent(requestBody, Encoding.UTF8, "text/xml");
+                using (var response = await client.PostAsync(uri, content))
+                {
+
+                }
+            }
+
+        }
+
+        public static async void On(int port)
+        {
+            string uri = $"http://192.168.1.{port}:49153/upnp/control/basicevent1";
+
+            string soapAction = "\"urn:Belkin:service:basicevent:1#SetBinaryState\"";
+
+            string requestBody = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n<s:Envelope xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\" s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\">\n <s:Body>\n  <u:SetBinaryState xmlns:u=\"urn:Belkin:service:basicevent:1\">\n   <BinaryState>1</BinaryState>\n  </u:SetBinaryState>\n </s:Body>\n</s:Envelope>";
+
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("SOAPAction", soapAction);
+                var content = new StringContent(requestBody, Encoding.UTF8, "text/xml");
+                using (var response = await client.PostAsync(uri, content))
+                {
+
+                }
+            }
+
+        }
+
+    }
+}
---
+++ rlara.prototypes.jarvis/rlara.prototypes.jarvis/StartupTask.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Background;
+using Windows.Media.Capture;
+using Windows.Media.SpeechRecognition;
+using rlara.prototypes.jarvis.Helpers;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace rlara.prototypes.jarvis
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        private BackgroundTaskDeferral _deferral;
+
+        private SpeechRecognizer _speechRecognizer;
+        private static bool _stayAlive = true;
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            _deferral = taskInstance.GetDeferral();
+
+            Debug.WriteLine(await MicTest());
+
+            _speechRecognizer = new SpeechRecognizer();
+
+            string[] responses = { "jarvis main light", "jarvis night light" };
+
+
+            var listConstraint = new Windows.Media.SpeechRecognition.SpeechRecognitionListConstraint(responses, "yesOrNo");
+
+            _speechRecognizer.Constraints.Add(listConstraint);
+
+            await _speechRecognizer.CompileConstraintsAsync();
+
+            _speechRecognizer.ContinuousRecognitionSession.ResultGenerated += ContinuousRecognitionSession_ResultGenerated;
+            await _speechRecognizer.ContinuousRecognitionSession.StartAsync();
+
+
+
+            while (_stayAlive)
+            {
+
+            }
+            _deferral.Complete();
+
+
+        }
+
+        private async Task<bool> MicTest()
+        {
+            try
+            {
+                MediaCaptureInitializationSettings init = new MediaCaptureInitializationSettings();
+                init.StreamingCaptureMode = StreamingCaptureMode.Audio;
+                init.MediaCategory = MediaCategory.Speech;
+                MediaCapture capture = new MediaCapture();
+
+                await capture.InitializeAsync(init);
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        private static void ContinuousRecognitionSession_ResultGenerated(
+            SpeechContinuousRecognitionSession sender,
+            SpeechContinuousRecognitionResultGeneratedEventArgs args)
+        {
+
+            Debug.WriteLine(args.Result.Text);
+
+            string mainlight = "jarvis main light";
+            string nightlight = "jarvis night light";
+
+            if (args.Result.Text.Contains(mainlight))
+            {
+
+                Wemo.Toggle(57);
+
+            }
+            else if (args.Result.Text.Contains(nightlight))
+            {
+
+                Wemo.Toggle(47);
+
+            }
+
+        }
+
+    }
+}
